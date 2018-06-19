1f981b8dea62309e7d766a95010b257a0bdb2234
Syed Faraaz Ahmad <faraaz98@live.com>
2018-06-19 17:41:01 +0530

    complete task 06

--- lib/category.dart
+++ lib/category.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
-import 'unit.dart';
-import 'converter_route.dart';
+// @required is defined in the meta.dart package
 import 'package:meta/meta.dart';
 
-const _rowHeight = 100.0;
-const _borderRadius = Radius.circular(_rowHeight / 16);
+import 'converter_route.dart';
+import 'unit.dart';
 
+// We use an underscore to indicate that these variables are private.
+// See https://www.dartlang.org/guides/language/effective-dart/design#libraries
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 16);
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
 class Category extends StatelessWidget {
-  final IconData iconLocation;
   final String name;
-  final Color color;
+  final ColorSwatch color;
+  final IconData iconLocation;
   final List<Unit> units;
 
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // While the @required checks for whether a named parameter is passed in,
+  // it doesn't check whether the object passed in is null. We check that
+  // in the assert statement.
   const Category({
+    Key key,
     @required this.name,
-    @required this.iconLocation,
     @required this.color,
+    @required this.iconLocation,
     @required this.units,
-  }) :
-      assert(name != null),
-      assert(iconLocation != null),
-      assert(color != null),
-      assert(units != null);
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        assert(units != null),
+        super(key: key);
 
+  /// Navigates to the [ConverterRoute].
   void _navigateToConverter(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute<Null>(
       builder: (BuildContext context) {
@@ -41,35 +58,59 @@ class Category extends StatelessWidget {
             name: name,
             units: units,
           ),
+          // This prevents the attempt to resize the screen when the keyboard
+          // is opened
+          resizeToAvoidBottomPadding: false,
         );
-      }
+      },
     ));
   }
 
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
   @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
       child: Container(
         height: _rowHeight,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: InkWell(
-            borderRadius: BorderRadius.all(_borderRadius),
-            highlightColor: color.withAlpha(125),
-            splashColor: color,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color['highlight'],
+          splashColor: color['splash'],
+          // We can use either the () => function() or the () { function(); }
+          // syntax.
+          onTap: () => _navigateToConverter(context),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
             child: Row(
-              children: <Widget>[
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              // There are two ways to denote a list: `[]` and `List()`.
+              // Prefer to use the literal syntax, i.e. `[]`, instead of `List()`.
+              // You can add the type argument if you'd like, i.e. <Widget>[].
+              // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
+              children: [
                 Padding(
                   padding: EdgeInsets.all(16.0),
-                  child: iconLocation != null ? Icon(iconLocation) : Icon(Icons.warning),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
                 ),
                 Center(
-                  child: Text(this.name, style: TextStyle(fontSize: 24.0)),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
                 ),
               ],
             ),
-            onTap: () => _navigateToConverter(context),
           ),
         ),
       ),
